8f2bdca6d0628c8faa93a561766731177d4a2de3
Jeffrey <jeffrey@vanbijleveld.nl>
2018-05-28 14:38:13 +0200

    refactoring

--- src/main/java/nl/vanbijleveld/imapreader/ImapService.java
+++ src/main/java/nl/vanbijleveld/imapreader/ImapService.java
@@ -1,18 +1,16 @@
 package nl.vanbijleveld.imapreader;
 
-import java.util.concurrent.Callable;
-
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 
-public class NewEmailService {
+public class ImapService {
 
     private final ImapFolderReader imapFolder;
     private Folder inbox;
     private final String PROCESSED_MAIL_FOLDER = "processed";
 
-    public NewEmailService(String mailServer, String userName, String password) throws MessagingException {
+    public ImapService(String mailServer, String userName, String password) throws MessagingException {
         imapFolder = new ImapFolderReader(mailServer, userName, password);
     }
 
@@ -21,16 +19,17 @@ public class NewEmailService {
         return inbox.getMessages();
     }
 
-    public void processNewMessages(Callable<Void> func) {
+    public void setProcessed(Message mail) throws MessagingException {
+        imapFolder.moveToFolder(mail, PROCESSED_MAIL_FOLDER);
+    }
+
+    public void processNewMessages() {
         Message[] newMessages;
         try {
             newMessages = checkForNewEmail();
             for (Message mail : newMessages) {
                 System.out.println("processing mail: " + mail.getSubject() + " received on " + mail.getReceivedDate() + " from " + mail.getFrom());
-
-                func.call();
-                imapFolder.moveToFolder(mail, PROCESSED_MAIL_FOLDER);
-
+                setProcessed(mail);
             }
         } catch (Exception e) {
             // TODO Auto-generated catch block
